7dbcdaaf96648a7f1b6fd0513c0e3bb79b2bfd66
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2020-07-09 13:46:43 -0400

    separate build from source

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 antora antora-playbook.yml
 antora --fetch antora-playbook.yml # updates UI
 ```
+
+## Publishing
+
+``` sh
+./bin/publish.sh
+```
---
+++ bin/publish.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+build_dir=sweet-tooth-clojure.github.io
+
+antora --fetch --to-dir=../$build_dir antora-playbook.yml
+cd $build_dir
+git add .
